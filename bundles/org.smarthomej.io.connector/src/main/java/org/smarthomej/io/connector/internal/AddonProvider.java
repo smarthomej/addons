caf1ceaa839e86ff8ea4c1e75dc07306dab4d705
Jan N. Klug <jan.n.klug@rub.de>
2021-02-18 18:28:14 +0100

    Initial contribution
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>

---
+++ bundles/org.smarthomej.io.connector/src/main/java/org/smarthomej/io/connector/internal/AddonProvider.java
@@ -0,0 +1,194 @@
+/**
+ * Copyright (c) 2021 Contributors to the SmartHome/J project
+ * <p>
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ * <p>
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * <p>
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.smarthomej.io.connector.internal;
+
+import java.net.URI;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import org.apache.karaf.features.Feature;
+import org.apache.karaf.features.FeaturesService;
+import org.apache.karaf.features.Repository;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.core.addon.Addon;
+import org.openhab.core.addon.AddonEventFactory;
+import org.openhab.core.addon.AddonService;
+import org.openhab.core.addon.AddonType;
+import org.openhab.core.events.EventPublisher;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link AddonProvider} is a service providing the SmartHome/J addons
+ * handlers.
+ *
+ * @author Jan N. Klug - Initial contribution
+ */
+@NonNullByDefault
+@Component(name = "org.smarthomej.addons", service = { AddonProvider.class, AddonService.class })
+public class AddonProvider implements AddonService {
+    private final Logger logger = LoggerFactory.getLogger(AddonProvider.class);
+
+    private final FeaturesService featuresService;
+    private final EventPublisher eventPublisher;
+
+    private Map<String, Addon> addons = Map.of();
+
+    @Activate
+    public AddonProvider(@Reference FeaturesService featuresService, @Reference EventPublisher eventPublisher) {
+        this.featuresService = featuresService;
+        this.eventPublisher = eventPublisher;
+
+        buildAddonList();
+
+        logger.debug("Addon provider started.");
+        logger.trace("Available addons: {}", addons);
+    }
+
+    /**
+     * add a feature repository from Addon provider (uninstalls all provided features)
+     *
+     * @param groupId
+     * @param artifactId
+     * @param version
+     * @return true if successfully added, false otherwise
+     */
+    public boolean addFeatureRepository(String groupId, String artifactId, String version) {
+        try {
+            featuresService.addRepository(uriFromCoordinates(groupId, artifactId, version));
+            logger.debug("Added feature repository '{}/{}/{}'", groupId, artifactId, version);
+            buildAddonList();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * remove a feature repository from Addon provider (also uninstalls all provided features)
+     *
+     * @param groupId
+     * @param artifactId
+     * @param version
+     * @return true if successfully removed, false otherwise
+     */
+    public boolean removeFeatureRepository(String groupId, String artifactId, String version) {
+        try {
+            featuresService.removeRepository(uriFromCoordinates(groupId, artifactId, version), true);
+            logger.debug("Removed feature repository '{}/{}/{}'", groupId, artifactId, version);
+            buildAddonList();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean statusFeatureRepository(String groupId, String artifactId, String version) {
+        try {
+            URI uri = uriFromCoordinates(groupId, artifactId, version);
+            return Arrays.stream(featuresService.listRepositories()).map(Repository::getURI).anyMatch(uri::equals);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private URI uriFromCoordinates(String groupId, String artifactId, String version) {
+        return URI.create("mvn:" + groupId + "/" + artifactId + "/" + version + "/xml/features");
+    }
+
+    private String addonIdFromFeature(Feature f) {
+        return f.getName() + "_" + f.getVersion().replace(".", "-");
+    }
+
+    private String featureIdFromAddonId(String addonId) {
+        return addonId.substring(0, addonId.indexOf("_"));
+    }
+
+    private void buildAddonList() {
+        try {
+            addons = Arrays.stream(featuresService.listFeatures())
+                    .filter(f -> f.getName().contains("smarthomej-binding"))
+                    .map(f -> new Addon(addonIdFromFeature(f), "smarthomej-binding", f.getDescription(), f.getVersion(),
+                            "https://github.com/smarthomej/addons/blob/main/bundles/org.smarthomej.binding."
+                                    + f.getName().replace("smarthomej-binding-", "") + "/README.md",
+                            featuresService.isInstalled(f), f.getDescription(), null, null))
+                    .collect(Collectors.toMap(Addon::getId, a -> a));
+        } catch (Exception e) {
+            logger.warn("Failed to build addon list: {}", e.getMessage());
+        }
+    }
+
+    @Override
+    @NonNullByDefault({})
+    public List<Addon> getAddons(@Nullable Locale locale) {
+        return new ArrayList<>(addons.values());
+    }
+
+    @Override
+    public @Nullable Addon getAddon(@Nullable String id, @Nullable Locale locale) {
+        return addons.get(id);
+    }
+
+    @Override
+    @NonNullByDefault({})
+    public List<AddonType> getTypes(@Nullable Locale locale) {
+        return List.of(new AddonType("smarthomej-binding", "SmartHome/J"));
+    }
+
+    @Override
+    public void install(@Nullable String id) {
+        if (id != null) {
+            try {
+                Addon addon = addons.get(id);
+                if (addon == null) {
+                    throw new IllegalArgumentException("No addon with found with id" + id);
+                }
+                featuresService.installFeature(featureIdFromAddonId(addon.getId()), addon.getVersion(),
+                        EnumSet.of(FeaturesService.Option.Upgrade, FeaturesService.Option.NoFailOnFeatureNotFound));
+
+                eventPublisher.post(AddonEventFactory.createAddonInstalledEvent(id));
+                logger.info("Installed {}", id);
+                addon.setInstalled(true);
+            } catch (Exception e) {
+                logger.warn("Failed to install {}: {}", id, e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void uninstall(@Nullable String id) {
+        if (id != null) {
+            try {
+                Addon addon = addons.get(id);
+                if (addon == null) {
+                    throw new IllegalArgumentException("No addon with found with id" + id);
+                }
+                featuresService.uninstallFeature(featureIdFromAddonId(addon.getId()), addon.getVersion());
+                addon.setInstalled(false);
+                eventPublisher.post(AddonEventFactory.createAddonUninstalledEvent(id));
+                logger.info("Uninstalled {}", id);
+            } catch (Exception e) {
+                logger.warn("Failed to uninstall {}: {}", id, e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public @Nullable String getAddonId(@Nullable URI addonURI) {
+        return null;
+    }
+}
---
+++ bundles/org.smarthomej.io.connector/src/main/java/org/smarthomej/io/connector/internal/ConnectorServlet.java
@@ -0,0 +1,184 @@
+/**
+ * Copyright (c) 2021 Contributors to the SmartHome/J project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.smarthomej.io.connector.internal;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link ConnectorServlet} provides a Servlet for controlling the connector
+ *
+ * @author Jan N. Klug - Initial contribution
+ */
+@Component(immediate = true)
+@NonNullByDefault
+public class ConnectorServlet extends HttpServlet {
+    private static final String SERVLET_URL = "/smarthomej";
+    private static final long serialVersionUID = 1L;
+
+    private static final String SNAPSHOT_REPO_ID = "@snapshots@id=smarthomej-snapshot";
+    private static final String SNAPSHOT_REPO_URL = "https://oss.sonatype.org/content/repositories/snapshots";
+    private static final String RELEASE_REPO_ID = "@id=smarthomej-release";
+    private static final String RELEASE_REPO_URL = "https://repo1.maven.org/maven2";
+
+    private static final Map<String, String> REPO_URLS = Map.of(SNAPSHOT_REPO_ID, SNAPSHOT_REPO_URL, RELEASE_REPO_ID,
+            RELEASE_REPO_URL);
+
+    public static final String KARAF_FEATURE_GROUP_ID = "org.smarthomej.addons.features.karaf";
+    public static final String KARAF_FEATURE_ARTIFACT_ID = "org.smarthomej.addons.features.karaf.smarthomej-addons";
+
+    private final Logger logger = LoggerFactory.getLogger(ConnectorServlet.class);
+
+    private final HttpService httpService;
+    private final MavenRepoManager mavenRepoManager;
+    private final AddonProvider addonProvider;
+
+    @Activate
+    public ConnectorServlet(@Reference HttpService httpService, @Reference MavenRepoManager mavenRepoManager,
+            @Reference AddonProvider addonProvider) {
+        this.httpService = httpService;
+        this.mavenRepoManager = mavenRepoManager;
+        this.addonProvider = addonProvider;
+
+        try {
+            httpService.registerServlet(SERVLET_URL, this, null, httpService.createDefaultHttpContext());
+        } catch (ServletException | NamespaceException e) {
+            logger.warn("Failed to install JSmartHome Connector servlet: {}", e.getMessage());
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @Deactivate
+    public void deactivate() {
+        stop();
+    }
+
+    @Override
+    protected void doGet(@Nullable HttpServletRequest req, @Nullable HttpServletResponse resp) throws IOException {
+        if (req == null || resp == null) {
+            return;
+        }
+
+        String requestUri = req.getRequestURI();
+        if (requestUri == null) {
+            return;
+        }
+        String uri = requestUri.substring(SERVLET_URL.length());
+        String queryString = req.getQueryString();
+
+        if (!uri.isEmpty()) {
+            doRepoAction(uri, queryString);
+            resp.sendRedirect(SERVLET_URL);
+            return;
+        }
+
+        StringBuilder html = new StringBuilder();
+        html.append("<html><head><title>SmartHome/J Connector</title><head><body>");
+        html.append("<h1>SmartHome/J Connector Configuration</h1>");
+
+        buildRepoEntry(html, "Snapshot", SNAPSHOT_REPO_ID);
+        buildRepoEntry(html, "Release", RELEASE_REPO_ID);
+
+        html.append("<hr/></body></html>");
+        resp.addHeader("content-type", "text/html;charset=UTF-8");
+        try {
+            resp.getWriter().write(html.toString());
+        } catch (IOException e) {
+            logger.warn("Return html failed with uri syntax error", e);
+        }
+    }
+
+    public void stop() {
+        httpService.unregister(SERVLET_URL);
+    }
+
+    private void doRepoAction(String uri, @Nullable String queryString) {
+        switch (uri) {
+            case "/removeMavenRepo":
+                if (queryString != null && !queryString.isEmpty()) {
+                    // remove all feature repos from this maven repository
+                    mavenRepoManager
+                            .getAvailableVersions(queryString, KARAF_FEATURE_GROUP_ID, KARAF_FEATURE_ARTIFACT_ID)
+                            .forEach(v -> addonProvider.removeFeatureRepository(KARAF_FEATURE_GROUP_ID,
+                                    KARAF_FEATURE_ARTIFACT_ID, v));
+                    mavenRepoManager.removeRepository(queryString);
+                }
+                break;
+            case "/addMavenRepo":
+                if (queryString != null && !queryString.isEmpty()) {
+                    String repoUrl = REPO_URLS.get(queryString);
+                    if (repoUrl != null) {
+                        mavenRepoManager.addRepository(queryString, repoUrl);
+                    } else {
+                        logger.warn("Could not find URL for maven repository '{}'", queryString);
+                    }
+                }
+                break;
+            case "/addFeatureRepo":
+                if (queryString != null && !queryString.isEmpty()) {
+                    addonProvider.addFeatureRepository(KARAF_FEATURE_GROUP_ID, KARAF_FEATURE_ARTIFACT_ID, queryString);
+                }
+                break;
+            case "/removeFeatureRepo":
+                if (queryString != null && !queryString.isEmpty()) {
+                    addonProvider.removeFeatureRepository(KARAF_FEATURE_GROUP_ID, KARAF_FEATURE_ARTIFACT_ID,
+                            queryString);
+                }
+                break;
+            default:
+                logger.warn("Unknown request: {}", uri);
+        }
+    }
+
+    private void buildRepoEntry(StringBuilder html, String repoName, String repoId) {
+        boolean mavenRepoInstalled = mavenRepoManager.repositoryStatus(repoId);
+        html.append("<hr/><h2>").append(repoName).append(":</h2><br />Status: ")
+                .append(mavenRepoInstalled ? "active" : "not active").append("<br/><br/>");
+
+        if (mavenRepoInstalled) {
+            mavenRepoManager.getAvailableVersions(repoId, KARAF_FEATURE_GROUP_ID, KARAF_FEATURE_ARTIFACT_ID).stream()
+                    .map(this::buildVersionLink).forEach(html::append);
+
+            html.append("<br/><a href=\"").append(SERVLET_URL).append("/removeMavenRepo?").append(repoId)
+                    .append("\">Remove</a>");
+        } else {
+            html.append("<a href=\"").append(SERVLET_URL).append("/addMavenRepo?").append(repoId).append("\">Add</a>");
+        }
+    }
+
+    private String buildVersionLink(String versionId) {
+        String versionLink = "<a href=\"" + SERVLET_URL;
+        if (addonProvider.statusFeatureRepository(KARAF_FEATURE_GROUP_ID, KARAF_FEATURE_ARTIFACT_ID, versionId)) {
+            versionLink += "/removeFeatureRepo?" + versionId + "\">" + versionId + " (active)</a><br/>";
+        } else {
+            versionLink += "/addFeatureRepo?" + versionId + "\">" + versionId + " (inactive)</a><br/>";
+        }
+        return versionLink;
+    }
+}
