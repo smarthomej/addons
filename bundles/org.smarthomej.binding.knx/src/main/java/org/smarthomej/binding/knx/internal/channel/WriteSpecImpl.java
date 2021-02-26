bc610751efe39fd2f02a81df1bcc49cd4d9da283
Jan N. Klug <jan.n.klug@rub.de>
2021-02-26 19:51:02 +0100

    add knx binding
    forked from https://github/openhab/openhab-addons 2021-02-26
    
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>

---
+++ bundles/org.smarthomej.binding.knx/src/main/java/org/smarthomej/binding/knx/internal/channel/WriteSpecImpl.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ * Copyright (c) 2021 Contributors to the SmartHome/J project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.smarthomej.binding.knx.internal.channel;
+
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.core.types.Type;
+import org.smarthomej.binding.knx.internal.client.OutboundSpec;
+
+import tuwien.auto.calimero.GroupAddress;
+import tuwien.auto.calimero.KNXFormatException;
+
+/**
+ * Command meta-data
+ *
+ * @author Simon Kaufmann - initial contribution and API.
+ *
+ */
+public class WriteSpecImpl extends AbstractSpec implements OutboundSpec {
+
+    private final Type type;
+    private final @Nullable GroupAddress groupAddress;
+
+    public WriteSpecImpl(@Nullable ChannelConfiguration channelConfiguration, String defaultDPT, Type type)
+            throws KNXFormatException {
+        super(channelConfiguration, defaultDPT);
+        if (channelConfiguration != null) {
+            this.groupAddress = new GroupAddress(channelConfiguration.getMainGA().getGA());
+        } else {
+            this.groupAddress = null;
+        }
+        this.type = type;
+    }
+
+    @Override
+    public Type getType() {
+        return type;
+    }
+
+    @Override
+    public @Nullable GroupAddress getGroupAddress() {
+        return groupAddress;
+    }
+}
---
+++ bundles/org.smarthomej.binding.knx/src/main/java/org/smarthomej/binding/knx/internal/client/InboundSpec.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ * Copyright (c) 2021 Contributors to the SmartHome/J project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.smarthomej.binding.knx.internal.client;
+
+import java.util.List;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+import tuwien.auto.calimero.GroupAddress;
+
+/**
+ * Describes the relevant parameters for reading from/listening to the KNX bus.
+ *
+ * @author Simon Kaufmann - initial contribution and API
+ *
+ */
+@NonNullByDefault
+public interface InboundSpec {
+
+    /**
+     * Get the datapoint type.
+     *
+     * @return the datapoint type
+     */
+    String getDPT();
+
+    /**
+     * Get the affected group addresses.
+     *
+     * @return a list of group addresses.
+     */
+    List<GroupAddress> getGroupAddresses();
+}
