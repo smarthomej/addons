bd44bc3b881aa7c2bc540c56792400517383615b
J-N-K <J-N-K@users.noreply.github.com>
2021-05-09 09:41:03 +0200

    [tcpudp] Fix client thing (#94)
    * fix ClientThingHandler
    * add tests
    
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>

---
+++ bundles/org.smarthomej.binding.tcpudp/src/test/java/org/smarthomej/binding/tcpudp/internal/ClientThingHandlerTest.java
@@ -0,0 +1,198 @@
+/**
+ * Copyright (c) 2021 Contributors to the SmartHome/J project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.smarthomej.binding.tcpudp.internal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.smarthomej.binding.tcpudp.internal.TcpUdpBindingConstants.BINDING_ID;
+import static org.smarthomej.binding.tcpudp.internal.TcpUdpBindingConstants.THING_TYPE_UID_CLIENT;
+
+import java.util.List;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.openhab.core.library.types.StringType;
+import org.openhab.core.test.java.JavaTest;
+import org.openhab.core.thing.Channel;
+import org.openhab.core.thing.ChannelUID;
+import org.openhab.core.thing.Thing;
+import org.openhab.core.thing.ThingStatus;
+import org.openhab.core.thing.ThingStatusDetail;
+import org.openhab.core.thing.ThingStatusInfo;
+import org.openhab.core.thing.ThingUID;
+import org.openhab.core.thing.binding.ThingHandlerCallback;
+import org.openhab.core.thing.binding.builder.ChannelBuilder;
+import org.openhab.core.thing.binding.builder.ThingBuilder;
+import org.openhab.core.thing.type.ChannelTypeUID;
+import org.smarthomej.binding.tcpudp.internal.config.ClientConfiguration;
+import org.smarthomej.binding.tcpudp.internal.config.TcpUdpChannelConfig;
+import org.smarthomej.binding.tcpudp.internal.test.EchoServer;
+import org.smarthomej.binding.tcpudp.internal.test.TestUtil;
+import org.smarthomej.commons.SimpleDynamicStateDescriptionProvider;
+import org.smarthomej.commons.itemvalueconverter.ChannelMode;
+import org.smarthomej.commons.transform.NoOpValueTransformation;
+import org.smarthomej.commons.transform.ValueTransformationProvider;
+
+/**
+ * The {@link ClientThingHandlerTest} is a test class for {@link ClientThingHandler}
+ *
+ * @author Jan N. Klug - Initial contribution
+ */
+@NonNullByDefault
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.WARN)
+public class ClientThingHandlerTest extends JavaTest {
+    private static final String TEST_STATE_CONTENT = "testStateContent";
+    private static final ThingUID TEST_THING_UID = new ThingUID(THING_TYPE_UID_CLIENT, "testThing");
+    private static final ChannelTypeUID CHANNEL_TYPE_UID = new ChannelTypeUID(BINDING_ID, "string");
+    private static final ChannelUID TEST_CHANNEL_UID = new ChannelUID(TEST_THING_UID, "testChannel");
+
+    @Mock
+    private @NonNullByDefault({}) ThingHandlerCallback thingHandlerCallback;
+
+    @Mock
+    private @NonNullByDefault({}) ValueTransformationProvider valueTransformationProvider;
+
+    @Mock
+    private @NonNullByDefault({}) SimpleDynamicStateDescriptionProvider simpleDynamicStateDescriptionProvider;
+
+    @BeforeEach
+    public void startUp() {
+        Mockito.doReturn(NoOpValueTransformation.getInstance()).when(valueTransformationProvider)
+                .getValueTransformation(any());
+    }
+
+    @Test
+    public void tcpRequestTest() {
+        requestTest(ClientConfiguration.Protocol.TCP);
+    }
+
+    @Test
+    public void udpRequestTest() {
+        requestTest(ClientConfiguration.Protocol.UDP);
+    }
+
+    @Test
+    public void udpSendTest() {
+        sendTest(ClientConfiguration.Protocol.UDP);
+    }
+
+    @Test
+    public void tcpSendTest() {
+        sendTest(ClientConfiguration.Protocol.TCP);
+    }
+
+    private void requestTest(ClientConfiguration.Protocol protocol) {
+        EchoServer echoServer = new EchoServer(protocol);
+        waitForAssert(() -> assertNotEquals(0, echoServer.getPort(), "Could not start EchoServer"));
+
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        clientConfiguration.host = "127.0.0.1";
+        clientConfiguration.port = echoServer.getPort();
+        clientConfiguration.refresh = 1;
+        clientConfiguration.protocol = protocol;
+
+        TcpUdpChannelConfig tcpUdpChannelConfig = new TcpUdpChannelConfig();
+        tcpUdpChannelConfig.stateContent = TEST_STATE_CONTENT;
+
+        ClientThingHandler clientThingHandler = getClientThingHandler(clientConfiguration, tcpUdpChannelConfig);
+
+        // check that the thing status is set to unknown
+        verify(thingHandlerCallback).statusUpdated(clientThingHandler.getThing(),
+                new ThingStatusInfo(ThingStatus.UNKNOWN, ThingStatusDetail.NONE, null));
+
+        // wait until we have at least three calls and stop the thing handler
+        waitForAssert(() -> assertEquals(3, echoServer.getReceivedValues().size()));
+        clientThingHandler.dispose();
+
+        // get the exact number of calls and check if the channel was updated the same number of times
+        int calls = echoServer.getReceivedValues().size();
+        verify(thingHandlerCallback, times(calls)).stateUpdated(eq(TEST_CHANNEL_UID),
+                eq(new StringType(TEST_STATE_CONTENT)));
+
+        echoServer.stop();
+    }
+
+    private void sendTest(ClientConfiguration.Protocol protocol) {
+        EchoServer echoServer = new EchoServer(protocol);
+        waitForAssert(() -> assertNotEquals(0, echoServer.getPort(), "Could not start EchoServer"));
+
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        clientConfiguration.host = "127.0.0.1";
+        clientConfiguration.port = echoServer.getPort();
+        clientConfiguration.protocol = protocol;
+
+        TcpUdpChannelConfig tcpUdpChannelConfig = new TcpUdpChannelConfig();
+        tcpUdpChannelConfig.mode = ChannelMode.WRITEONLY;
+
+        ClientThingHandler clientThingHandler = getClientThingHandler(clientConfiguration, tcpUdpChannelConfig);
+
+        clientThingHandler.handleCommand(TEST_CHANNEL_UID, new StringType(TEST_STATE_CONTENT));
+
+        // check that the thing status is set to unknown
+        verify(thingHandlerCallback).statusUpdated(clientThingHandler.getThing(),
+                new ThingStatusInfo(ThingStatus.UNKNOWN, ThingStatusDetail.NONE, null));
+
+        // wait until we have at least one calls and stop the thing handler
+        waitForAssert(() -> assertEquals(1, echoServer.getReceivedValues().size()));
+        clientThingHandler.dispose();
+
+        // check the contents are all what we send
+        List<String> receivedValues = echoServer.getReceivedValues();
+        assertTrue(receivedValues.stream().allMatch(TEST_STATE_CONTENT::equals));
+
+        // no state updates
+        verify(thingHandlerCallback, never()).stateUpdated(any(), any());
+
+        echoServer.stop();
+    }
+
+    /**
+     * create a ClientThingHandler and initialize it
+     *
+     * @param clientConfiguration the thing configuration
+     * @param tcpUdpChannelConfig the channel configuration
+     * @return the initialized ClientThingHandler
+     */
+    private ClientThingHandler getClientThingHandler(ClientConfiguration clientConfiguration,
+            TcpUdpChannelConfig tcpUdpChannelConfig) {
+        Channel channel = ChannelBuilder.create(TEST_CHANNEL_UID).withAcceptedItemType("String")
+                .withType(CHANNEL_TYPE_UID)
+                .withConfiguration(TestUtil.getConfigurationFromInstance(tcpUdpChannelConfig)).build();
+        Thing thing = ThingBuilder.create(THING_TYPE_UID_CLIENT, TEST_THING_UID)
+                .withConfiguration(TestUtil.getConfigurationFromInstance(clientConfiguration)).withChannel(channel)
+                .build();
+
+        ClientThingHandler testClientThingHandler = new ClientThingHandler(thing, valueTransformationProvider,
+                simpleDynamicStateDescriptionProvider);
+
+        testClientThingHandler.setCallback(thingHandlerCallback);
+
+        testClientThingHandler.initialize();
+
+        return testClientThingHandler;
+    }
+}
---
+++ bundles/org.smarthomej.binding.tcpudp/src/test/java/org/smarthomej/binding/tcpudp/internal/test/EchoServer.java
@@ -0,0 +1,130 @@
+/**
+ * Copyright (c) 2021 Contributors to the SmartHome/J project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.smarthomej.binding.tcpudp.internal.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smarthomej.binding.tcpudp.internal.config.ClientConfiguration;
+
+/**
+ * The {@link EchoServer} is an echo server for UDP or TCP connections
+ *
+ * @author Jan N. Klug - Initial contribution
+ */
+@NonNullByDefault
+public class EchoServer {
+    private final Logger logger = LoggerFactory
+            .getLogger(org.smarthomej.binding.tcpudp.internal.receiver.TcpReceiver.class);
+
+    private final List<String> receivedValues = new ArrayList<>();
+    private final Thread thread;
+
+    private @Nullable ServerSocket tcpSocket;
+    private @Nullable DatagramSocket udpSocket;
+
+    private int port = 0;
+    private byte[] buf = new byte[2048];
+
+    public EchoServer(ClientConfiguration.Protocol protocol) {
+        if (protocol == ClientConfiguration.Protocol.TCP) {
+            thread = new Thread(this::runTcp);
+        } else {
+            thread = new Thread(this::runUdp);
+        }
+        thread.start();
+    }
+
+    /**
+     * get the port that this instance listens to
+     *
+     * @return the port (0 if not listening)
+     */
+    public int getPort() {
+        return port;
+    }
+
+    public List<String> getReceivedValues() {
+        return receivedValues;
+    }
+
+    private void runUdp() {
+        try (DatagramSocket socket = new DatagramSocket(null)) {
+            this.udpSocket = socket;
+            socket.setReuseAddress(true);
+            socket.bind(new InetSocketAddress("0.0.0.0", 0));
+            port = socket.getLocalPort();
+            while (1 == 1) {
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+                byte[] data = Arrays.copyOfRange(packet.getData(), 0, packet.getLength());
+                receivedValues.add(new String(data));
+                socket.send(packet);
+            }
+        } catch (IOException e) {
+        }
+    }
+
+    private void runTcp() {
+        try (ServerSocket serverSocket = new ServerSocket()) {
+            this.tcpSocket = serverSocket;
+            serverSocket.setReuseAddress(true);
+            serverSocket.bind(new InetSocketAddress("0.0.0.0", 0));
+            port = serverSocket.getLocalPort();
+            while (1 == 1) {
+                try (Socket clientSocket = serverSocket.accept();
+                        InputStream in = clientSocket.getInputStream();
+                        OutputStream out = clientSocket.getOutputStream()) {
+                    int byteCount = in.read(buf);
+                    if (byteCount == -1) {
+                        logger.warn("Did not receive data");
+                    } else {
+                        byte[] data = Arrays.copyOfRange(buf, 0, byteCount);
+                        receivedValues.add(new String(data));
+                        out.write(data);
+                        out.flush();
+                    }
+                }
+            }
+        } catch (IOException e) {
+        }
+    }
+
+    public void stop() {
+        ServerSocket tcpSocket = this.tcpSocket;
+        if (tcpSocket != null) {
+            try {
+                tcpSocket.close();
+            } catch (IOException e) {
+                logger.warn("Could not close connection: {}", e.getMessage());
+            }
+        }
+        DatagramSocket udpSocket = this.udpSocket;
+        if (udpSocket != null) {
+            udpSocket.close();
+        }
+    }
+}
