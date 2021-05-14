66bc90bdcc9cecea4388311bd543c1c6ea190ed6
Christoph Weitkamp <github@christophweitkamp.de>
2021-05-14 18:12:21 +0200

    [transform.math] Initial contribution of Math Transformation Service (#101)
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>

---
+++ bundles/org.smarthomej.transform.math/src/test/java/org/smarthomej/binding/math/internal/MultiplyTransformationServiceTest.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2021 Contributors to the SmartHome/J project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.smarthomej.binding.math.internal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.junit.jupiter.api.Test;
+import org.openhab.core.transform.TransformationException;
+import org.openhab.core.transform.TransformationService;
+
+/**
+ * Unit test for {@link MultiplyTransformationService}.
+ *
+ * @author Christoph Weitkamp - Initial contribution
+ */
+@NonNullByDefault
+class MultiplyTransformationServiceTest {
+    private final TransformationService subject = new MultiplyTransformationService();
+
+    @Test
+    public void testTransform() throws TransformationException {
+        String result = subject.transform("20", "100");
+
+        assertEquals("2000", result);
+    }
+
+    @Test
+    public void testTransformInsideString() throws TransformationException {
+        String result = subject.transform("-20", "-0.5 watt");
+
+        assertEquals("10.0 watt", result);
+    }
+
+    public void testTransformInvalidSource() {
+        assertThrows(TransformationException.class, () -> subject.transform("20", "*"));
+    }
+
+    public void testTransformInvalidFunction() {
+        assertThrows(TransformationException.class, () -> subject.transform("*", "90"));
+    }
+}
