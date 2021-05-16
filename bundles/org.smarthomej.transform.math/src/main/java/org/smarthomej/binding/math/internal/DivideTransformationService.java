289994e4c5fea1eec1a2c4f9dcebb0273c330ada
Jan N. Klug <jan.n.klug@rub.de>
2021-05-16 17:59:36 +0200

    more universal exception handling
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>

--- bundles/org.smarthomej.transform.math/src/main/java/org/smarthomej/binding/math/internal/DivideTransformationService.java
+++ bundles/org.smarthomej.transform.math/src/main/java/org/smarthomej/binding/math/internal/DivideTransformationService.java
@@ -15,7 +15,6 @@ package org.smarthomej.binding.math.internal;
 import java.math.BigDecimal;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.openhab.core.transform.TransformationException;
 import org.openhab.core.transform.TransformationService;
 import org.osgi.service.component.annotations.Component;
 
@@ -29,11 +28,7 @@ import org.osgi.service.component.annotations.Component;
 public class DivideTransformationService extends AbstractMathTransformationService {
 
     @Override
-    BigDecimal performCalculation(BigDecimal source, BigDecimal value) throws TransformationException {
-        try {
-            return source.divide(value);
-        } catch (ArithmeticException e) {
-            throw new TransformationException("ArithmeticException: " + e.getMessage());
-        }
+    BigDecimal performCalculation(BigDecimal source, BigDecimal value) {
+        return source.divide(value);
     }
 }
