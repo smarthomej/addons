ab96cfa1ceeee6da6320a1930a55cb4229719f6a
J-N-K <J-N-K@users.noreply.github.com>
2021-05-16 17:30:20 +0200

    [transform.chain] Initial contribution (#97)
    * Initial contribution
    
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>

---
+++ bundles/org.smarthomej.transform.chain/src/main/java/org/smarthomej/transform/chain/internal/ChainTransformationProfile.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (c) 2021 Contributors to the SmartHome/J project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.smarthomej.transform.chain.internal;
+
+import java.util.Optional;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.core.library.types.StringType;
+import org.openhab.core.thing.profiles.ProfileCallback;
+import org.openhab.core.thing.profiles.ProfileContext;
+import org.openhab.core.thing.profiles.ProfileTypeUID;
+import org.openhab.core.thing.profiles.StateProfile;
+import org.openhab.core.transform.TransformationService;
+import org.openhab.core.types.Command;
+import org.openhab.core.types.State;
+import org.openhab.core.types.UnDefType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smarthomej.commons.transform.NoOpValueTransformation;
+import org.smarthomej.commons.transform.ValueTransformation;
+import org.smarthomej.commons.transform.ValueTransformationProvider;
+
+/**
+ * Profile to offer the ChainTransformation on a ItemChannelLink
+ *
+ * @author Jan N. Klug - Initial contribution
+ */
+@NonNullByDefault
+public class ChainTransformationProfile implements StateProfile {
+
+    public static final ProfileTypeUID PROFILE_TYPE_UID = new ProfileTypeUID(
+            TransformationService.TRANSFORM_PROFILE_SCOPE, "CHAIN");
+
+    private final Logger logger = LoggerFactory.getLogger(ChainTransformationProfile.class);
+    private final ProfileCallback callback;
+
+    private final ValueTransformationProvider valueTransformationProvider;
+    private final ChainTransformationProfileConfiguration configuration;
+
+    private ValueTransformation toItem;
+    private ValueTransformation toChannel;
+
+    public ChainTransformationProfile(ProfileCallback callback, ProfileContext context,
+            ValueTransformationProvider valueTransformationProvider) {
+        this.callback = callback;
+        this.valueTransformationProvider = valueTransformationProvider;
+
+        configuration = context.getConfiguration().as(ChainTransformationProfileConfiguration.class);
+        logger.debug("Profile configured with: '{}'", configuration);
+
+        toItem = valueTransformationProvider.getValueTransformation(configuration.toItem);
+        toChannel = valueTransformationProvider.getValueTransformation(configuration.toChannel);
+    }
+
+    @Override
+    public ProfileTypeUID getProfileTypeUID() {
+        return PROFILE_TYPE_UID;
+    }
+
+    @Override
+    public void onStateUpdateFromItem(State state) {
+        // do nothing
+    }
+
+    @Override
+    public void onCommandFromItem(Command command) {
+        transformToChannel(command.toString()).ifPresent(callback::handleCommand);
+    }
+
+    @Override
+    public void onCommandFromHandler(Command command) {
+        transformToItem(command.toString()).ifPresentOrElse(callback::sendCommand, () -> {
+            if (configuration.undefOnError) {
+                callback.sendUpdate(UnDefType.UNDEF);
+            }
+        });
+    }
+
+    @Override
+    public void onStateUpdateFromHandler(State state) {
+        transformToItem(state.toString()).ifPresentOrElse(callback::sendUpdate, () -> {
+            if (configuration.undefOnError) {
+                callback.sendUpdate(UnDefType.UNDEF);
+            }
+        });
+    }
+
+    private Optional<StringType> transformToItem(String input) {
+        if (!NoOpValueTransformation.getInstance().equals(toItem) && !configuration.toItem.isEmpty()) {
+            toItem = valueTransformationProvider.getValueTransformation(configuration.toItem);
+        }
+        return toItem.apply(input).map(StringType::new);
+    }
+
+    private Optional<StringType> transformToChannel(String input) {
+        if (!NoOpValueTransformation.getInstance().equals(toChannel) && !configuration.toChannel.isEmpty()) {
+            toChannel = valueTransformationProvider.getValueTransformation(configuration.toChannel);
+        }
+        return toChannel.apply(input).map(StringType::new);
+    }
+}
---
+++ bundles/org.smarthomej.transform.chain/src/main/java/org/smarthomej/transform/chain/internal/ChainTransformationProfileFactory.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2021 Contributors to the SmartHome/J project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.smarthomej.transform.chain.internal;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.core.thing.profiles.Profile;
+import org.openhab.core.thing.profiles.ProfileCallback;
+import org.openhab.core.thing.profiles.ProfileContext;
+import org.openhab.core.thing.profiles.ProfileFactory;
+import org.openhab.core.thing.profiles.ProfileType;
+import org.openhab.core.thing.profiles.ProfileTypeBuilder;
+import org.openhab.core.thing.profiles.ProfileTypeProvider;
+import org.openhab.core.thing.profiles.ProfileTypeUID;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.smarthomej.commons.transform.ValueTransformationProvider;
+
+/**
+ *
+ * A Factory that creates the transformation profiles for the {@link ChainTransformationProfile}
+ *
+ * @author Jan N. Klug - Initial contribution
+ */
+@NonNullByDefault
+@Component(service = { ProfileFactory.class, ProfileTypeProvider.class })
+public class ChainTransformationProfileFactory implements ProfileFactory, ProfileTypeProvider {
+    private final ValueTransformationProvider valueTransformationProvider;
+
+    @Activate
+    public ChainTransformationProfileFactory(@Reference ValueTransformationProvider valueTransformationProvider) {
+        this.valueTransformationProvider = valueTransformationProvider;
+    }
+
+    @Override
+    public Collection<ProfileType> getProfileTypes(@Nullable Locale locale) {
+        return List.of(ProfileTypeBuilder.newState(ChainTransformationProfile.PROFILE_TYPE_UID,
+                ChainTransformationProfile.PROFILE_TYPE_UID.getId()).build());
+    }
+
+    @Override
+    public @Nullable Profile createProfile(ProfileTypeUID profileTypeUID, ProfileCallback callback,
+            ProfileContext profileContext) {
+        return ChainTransformationProfile.PROFILE_TYPE_UID.equals(profileTypeUID)
+                ? new ChainTransformationProfile(callback, profileContext, valueTransformationProvider)
+                : null;
+    }
+
+    @Override
+    public Collection<ProfileTypeUID> getSupportedProfileTypeUIDs() {
+        return List.of(ChainTransformationProfile.PROFILE_TYPE_UID);
+    }
+}
