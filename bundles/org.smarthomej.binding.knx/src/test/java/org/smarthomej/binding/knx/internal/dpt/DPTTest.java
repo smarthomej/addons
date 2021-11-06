a8c046aac6f962d5b14528bb76f4307c396786ea
J-N-K <J-N-K@users.noreply.github.com>
2021-11-06 18:29:04 +0100

    [knx] fix overflow in DPT conversion (#219)
    * [knx] fix overflow in DPT conversion
    * fix percenttype for color position channel
    
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>

--- bundles/org.smarthomej.binding.knx/src/test/java/org/smarthomej/binding/knx/internal/dpt/DPTTest.java
+++ bundles/org.smarthomej.binding.knx/src/test/java/org/smarthomej/binding/knx/internal/dpt/DPTTest.java
@@ -15,6 +15,7 @@ package org.smarthomej.binding.knx.internal.dpt;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -91,5 +92,15 @@ public class DPTTest {
         String valueStr = "1 " + unit;
         QuantityType<?> value = new QuantityType<>(valueStr);
         Assertions.assertNotNull(value);
+    }
+
+    private static Stream<String> rgbValueProvider() {
+        return Stream.of("r:0 g:0 b:0", "r:255 g:255 b:255");
+    }
+
+    @ParameterizedTest
+    @MethodSource("rgbValueProvider")
+    public void rgbTest(String value) {
+        Assertions.assertNotNull(ValueDecoder.decode("232.600", value.getBytes(StandardCharsets.UTF_8), HSBType.class));
     }
 }
