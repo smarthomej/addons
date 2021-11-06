fb1b8e79008eb8dc8686ff4b340c02607ffa8992
Jan N. Klug <jan.n.klug@rub.de>
2021-11-06 13:25:34 +0100

    [knx] fix overflow in DPT conversion
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>

--- bundles/org.smarthomej.binding.knx/src/main/java/org/smarthomej/binding/knx/internal/dpt/ValueDecoder.java
+++ bundles/org.smarthomej.binding.knx/src/main/java/org/smarthomej/binding/knx/internal/dpt/ValueDecoder.java
@@ -274,8 +274,10 @@ public class ValueDecoder {
                     return HSBType.fromRGB(r, g, b);
                 case "60000":
                     // MDT specific: mis-use 232.600 for hsv instead of rgb
-                    return new HSBType(new DecimalType(r * 360.0 / 255.0), new PercentType(new BigDecimal(g / 2.55)),
-                            new PercentType(new BigDecimal(b / 2.55)));
+                    DecimalType hue = new DecimalType(coerceToRange(r * 360.0 / 255.0, 0.0, 360.0));
+                    PercentType sat = new PercentType(BigDecimal.valueOf(coerceToRange(g / 2.55, 0.0, 100.0)));
+                    PercentType bright = new PercentType(BigDecimal.valueOf(coerceToRange(b / 2.55, 0.0, 100.0)));
+                    return new HSBType(hue, sat, bright);
                 default:
                     LOGGER.warn("Unknown subtype '232.{}', no conversion possible.", subType);
                     return null;
@@ -317,9 +319,9 @@ public class ValueDecoder {
 
             if (rString != null && gString != null && bString != null && HSBType.class.equals(preferredType)) {
                 // does not support PercentType and r,g,b valid -> HSBType
-                int r = (int) (Integer.parseInt(rString) * 2.56);
-                int g = (int) (Integer.parseInt(gString) * 2.56);
-                int b = (int) (Integer.parseInt(bString) * 2.56);
+                int r = coerceToRange((int) (Integer.parseInt(rString) * 2.55), 0, 255);
+                int g = coerceToRange((int) (Integer.parseInt(gString) * 2.55), 0, 255);
+                int b = coerceToRange((int) (Integer.parseInt(bString) * 2.55), 0, 255);
 
                 return HSBType.fromRGB(r, g, b);
             } else if (wString != null && PercentType.class.equals(preferredType)) {
@@ -352,5 +354,13 @@ public class ValueDecoder {
         }
         LOGGER.warn("Failed to convert '{}' (DPT '{}'): no matching type found", value, id);
         return null;
+    }
+
+    private static double coerceToRange(double value, double min, double max) {
+        return Math.min(Math.max(value, min), max);
+    }
+
+    private static int coerceToRange(int value, int min, int max) {
+        return Math.min(Math.max(value, min), max);
     }
 }
