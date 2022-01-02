smarthomej/addons

commit 68f7de09f3c40648d36ad2c2909213134e09b068

[amazonechocontrol] code improvements (#237)
* refactor header handling
* better null handling
* refactor exception handling
* address compiler warnings
* extract classes and login from connection
* better error messages

Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>

bundles/org.smarthomej.binding.amazonechocontrol/src/main/java/org/smarthomej/binding/amazonechocontrol/internal/connection/LoginData.java
@@ -1,0 +1,222 @@
+/**
+ * Copyright (c) 2021 Contributors to the SmartHome/J project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.smarthomej.binding.amazonechocontrol.internal.connection;
+
+import java.net.CookieManager;
+import java.net.CookieStore;
+import java.net.HttpCookie;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.core.util.HexUtils;
+
+/**
+ * The {@link LoginData} holds the login data and provides the methods for serialization and deserialization
+ *
+ * @author Jan N. Klug - Initial contribution
+ */
+@NonNullByDefault
+public class LoginData {
+    private static final String DEVICE_TYPE = "A2IVLV5VM2W81";
+
+    private final Random rand = new Random();
+    private final CookieManager cookieManager;
+
+    // data fields
+    public String frc;
+    public String serial;
+    public String deviceId;
+    public @Nullable String refreshToken;
+    public String amazonSite = "amazon.com";
+    public String deviceName = "Unknown";
+    public @Nullable String accountCustomerId;
+    public @Nullable Date loginTime;
+    private List<Cookie> cookies = new ArrayList<>();
+
+    public LoginData(CookieManager cookieManager, String frc, String serial, String deviceId) {
+        this.cookieManager = cookieManager;
+        this.frc = frc;
+        this.serial = serial;
+        this.deviceId = deviceId;
+    }
+
+    public LoginData(CookieManager cookieManager) {
+        this.cookieManager = cookieManager;
+
+        // FRC
+        byte[] frcBinary = new byte[313];
+        rand.nextBytes(frcBinary);
+        this.frc = Base64.getEncoder().encodeToString(frcBinary);
+
+        // Serial number
+        byte[] serialBinary = new byte[16];
+        rand.nextBytes(serialBinary);
+        this.serial = HexUtils.bytesToHex(serialBinary);
+
+        // Device id 16 random bytes in upper-case hex format, a # as separator and a fixed DEVICE_TYPE
+        byte[] bytes = new byte[16];
+        rand.nextBytes(bytes);
+        String hexStr = HexUtils.bytesToHex(bytes).toUpperCase() + "#" + DEVICE_TYPE;
+        this.deviceId = HexUtils.bytesToHex(hexStr.getBytes());
+    }
+
+    public String serializeLoginData() {
+        Date loginTime = this.loginTime;
+        if (refreshToken == null || loginTime == null) {
+            return "";
+        }
+        StringBuilder builder = new StringBuilder();
+        builder.append("7\n"); // version
+        builder.append(frc).append("\n");
+        builder.append(serial).append("\n");
+        builder.append(deviceId).append("\n");
+        builder.append(refreshToken).append("\n");
+        builder.append(amazonSite).append("\n");
+        builder.append(deviceName).append("\n");
+        builder.append(accountCustomerId).append("\n");
+        builder.append(loginTime.getTime()).append("\n");
+        builder.append(cookies.size()).append("\n");
+        cookies = cookieManager.getCookieStore().getCookies().stream().map(LoginData.Cookie::fromHttpCookie)
+                .collect(Collectors.toList());
+        cookies.forEach(cookie -> builder.append(cookie.serialize()));
+        return builder.toString();
+    }
+
+    public boolean deserialize(String data) {
+        Scanner scanner = new Scanner(data);
+        String version = scanner.nextLine();
+        // check if serialize version is supported
+        if (!"7".equals(version)) {
+            scanner.close();
+            return false;
+        }
+
+        frc = scanner.nextLine();
+        serial = scanner.nextLine();
+        deviceId = scanner.nextLine();
+
+        refreshToken = scanner.nextLine();
+        amazonSite = scanner.nextLine();
+        deviceName = scanner.nextLine();
+        accountCustomerId = scanner.nextLine();
+        loginTime = new Date(Long.parseLong(scanner.nextLine()));
+
+        int numberOfCookies = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < numberOfCookies; i++) {
+            cookies.add(Cookie.fromScanner(scanner));
+        }
+        scanner.close();
+
+        CookieStore cookieStore = cookieManager.getCookieStore();
+        cookieStore.removeAll();
+        cookies.forEach(cookie -> cookieStore.add(null, cookie.toHttpCookie()));
+
+        return true;
+    }
+
+    private static class Cookie {
+        private final String name;
+        private final String value;
+        private final String comment;
+        private final String commentURL;
+        private final String domain;
+        private final long maxAge;
+        private final String path;
+        private final String portlist;
+        private final int version;
+        private final boolean secure;
+        private final boolean discard;
+
+        private Cookie(String name, String value, String comment, String commentURL, String domain, long maxAge,
+                String path, String portlist, int version, boolean secure, boolean discard) {
+            this.name = name;
+            this.value = value;
+            this.comment = comment;
+            this.commentURL = commentURL;
+            this.domain = domain;
+            this.maxAge = maxAge;
+            this.path = path;
+            this.portlist = portlist;
+            this.version = version;
+            this.secure = secure;
+            this.discard = discard;
+        }
+
+        private static String readValue(Scanner scanner) {
+            if (scanner.nextLine().equals("1")) {
+                return Objects.requireNonNullElse(scanner.nextLine(), "");
+            }
+            return "";
+        }
+
+        private void writeValue(StringBuilder builder, @Nullable Object value) {
+            if (value == null) {
+                builder.append("0\n");
+            } else {
+                builder.append("1").append("\n").append(value).append("\n");
+            }
+        }
+
+        public static Cookie fromScanner(Scanner scanner) {
+            return new Cookie(readValue(scanner), readValue(scanner), readValue(scanner), readValue(scanner),
+                    readValue(scanner), Long.parseLong(readValue(scanner)), readValue(scanner), readValue(scanner),
+                    Integer.parseInt(readValue(scanner)), Boolean.parseBoolean(readValue(scanner)),
+                    Boolean.parseBoolean(readValue(scanner)));
+        }
+
+        public String serialize() {
+            StringBuilder builder = new StringBuilder();
+            writeValue(builder, name);
+            writeValue(builder, value);
+            writeValue(builder, comment);
+            writeValue(builder, commentURL);
+            writeValue(builder, domain);
+            writeValue(builder, maxAge);
+            writeValue(builder, path);
+            writeValue(builder, portlist);
+            writeValue(builder, version);
+            writeValue(builder, secure);
+            writeValue(builder, discard);
+
+            return builder.toString();
+        }
+
+        public static Cookie fromHttpCookie(HttpCookie cookie) {
+            return new Cookie(cookie.getName(), cookie.getValue(), cookie.getComment(), cookie.getCommentURL(),
+                    cookie.getDomain(), cookie.getMaxAge(), cookie.getPath(), cookie.getPortlist(), cookie.getVersion(),
+                    cookie.getSecure(), cookie.getDiscard());
+        }
+
+        public HttpCookie toHttpCookie() {
+            HttpCookie clientCookie = new HttpCookie(name, value);
+            clientCookie.setComment(comment);
+            clientCookie.setCommentURL(commentURL);
+            clientCookie.setDomain(domain);
+            clientCookie.setMaxAge(maxAge);
+            clientCookie.setPath(path);
+            clientCookie.setPortlist(portlist);
+            clientCookie.setVersion(version);
+            clientCookie.setSecure(secure);
+            clientCookie.setDiscard(discard);
+            return clientCookie;
+        }
+    }
+}

bundles/org.smarthomej.binding.amazonechocontrol/src/main/java/org/smarthomej/binding/amazonechocontrol/internal/AccountServlet.java
@@ -39,6 +39,7 @@ import org.osgi.service.http.HttpService;
 import org.osgi.service.http.NamespaceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.smarthomej.binding.amazonechocontrol.internal.connection.Connection;
 import org.smarthomej.binding.amazonechocontrol.internal.handler.AccountHandler;
 import org.smarthomej.binding.amazonechocontrol.internal.jsons.JsonBluetoothStates;
 import org.smarthomej.binding.amazonechocontrol.internal.jsons.JsonBluetoothStates.BluetoothState;
@@ -155,7 +156,7 @@ public class AccountServlet extends HttpServlet {
                 return;
             }
             String domain = domainArray[0];
-            String loginData = connection.serializeLoginData();
+            String loginData = connection.getLoginData().serializeLoginData();
             Connection newConnection = new Connection(null, this.gson);
             if (newConnection.tryRestoreLogin(loginData, domain)) {
                 account.setConnection(newConnection);
@@ -328,7 +329,7 @@ public class AccountServlet extends HttpServlet {
 
             String html = connection.getLoginPage();
             returnHtml(connection, resp, html, "amazon.com");
-        } catch (URISyntaxException | InterruptedException e) {
+        } catch (URISyntaxException | ConnectionException e) {
             logger.warn("get failed with uri syntax error", e);
         }
     }
@@ -436,8 +437,7 @@ public class AccountServlet extends HttpServlet {
         createPageEndAndSent(resp, html);
     }
 
-    private void handleDevices(HttpServletResponse resp, Connection connection)
-            throws IOException, URISyntaxException, InterruptedException {
+    private void handleDevices(HttpServletResponse resp, Connection connection) throws ConnectionException {
         returnHtml(connection, resp, "<html>" + HtmlEscape.escapeHtml4(connection.getDeviceListJson()) + "</html>");
     }
 
@@ -532,8 +532,7 @@ public class AccountServlet extends HttpServlet {
         String errorMessage = "No notifications sounds found";
         try {
             notificationSounds = connection.getNotificationSounds(device);
-        } catch (IOException | HttpException | URISyntaxException | JsonSyntaxException | ConnectionException
-                | InterruptedException e) {
+        } catch (ConnectionException | JsonSyntaxException e) {
             errorMessage = e.getLocalizedMessage();
         }
         if (!notificationSounds.isEmpty()) {
@@ -564,8 +563,7 @@ public class AccountServlet extends HttpServlet {
         String errorMessage = "No playlists found";
         try {
             playLists = connection.getPlaylists(device);
-        } catch (IOException | HttpException | URISyntaxException | JsonSyntaxException | ConnectionException
-                | InterruptedException e) {
+        } catch (ConnectionException | JsonSyntaxException e) {
             errorMessage = e.getLocalizedMessage();
         }
 
@@ -578,7 +576,7 @@ public class AccountServlet extends HttpServlet {
                     {
                         if (innerLists != null && innerLists.length > 0) {
                             PlayList playList = innerLists[0];
-                            if (playList != null && playList.playlistId != null && playList.title != null) {
+                            if (playList.playlistId != null && playList.title != null) {
                                 html.append("<tr><td>");
                                 html.append(HtmlEscape.escapeHtml4(nullReplacement(playList.title)));
                                 html.append("</td><td>");
@@ -598,9 +596,6 @@ public class AccountServlet extends HttpServlet {
     private void renderBluetoothMacChannel(Connection connection, Device device, StringBuilder html) {
         html.append("<h2>").append(HtmlEscape.escapeHtml4("Channel " + CHANNEL_BLUETOOTH_MAC)).append("</h2>");
         JsonBluetoothStates bluetoothStates = connection.getBluetoothConnectionStates();
-        if (bluetoothStates == null) {
-            return;
-        }
         BluetoothState[] innerStates = bluetoothStates.bluetoothStates;
         if (innerStates == null) {
             return;
@@ -634,9 +629,8 @@ public class AccountServlet extends HttpServlet {
             @Nullable String referer, @Nullable String postData, boolean json, String site) throws IOException {
         HttpsURLConnection urlConnection;
         try {
-            Map<String, String> headers = null;
+            Map<String, String> headers = new HashMap<>();
             if (referer != null) {
-                headers = new HashMap<>();
                 headers.put("Referer", referer);
             }
 
@@ -681,12 +675,11 @@ public class AccountServlet extends HttpServlet {
                     return;
                 }
             }
-        } catch (URISyntaxException | ConnectionException | InterruptedException e) {
+            String response = connection.convertStream(urlConnection);
+            returnHtml(connection, resp, response, site);
+        } catch (ConnectionException | InterruptedException e) {
             returnError(resp, e.getLocalizedMessage());
-            return;
         }
-        String response = connection.convertStream(urlConnection);
-        returnHtml(connection, resp, response, site);
     }
 
     private void returnHtml(Connection connection, HttpServletResponse resp, String html) {

bundles/org.smarthomej.binding.amazonechocontrol/src/main/java/org/smarthomej/binding/amazonechocontrol/internal/AmazonEchoControlHandlerFactory.java
@@ -36,8 +36,6 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.http.HttpService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.smarthomej.binding.amazonechocontrol.internal.handler.AccountHandler;
 import org.smarthomej.binding.amazonechocontrol.internal.handler.EchoHandler;
 import org.smarthomej.binding.amazonechocontrol.internal.handler.FlashBriefingProfileHandler;
@@ -55,8 +53,6 @@ import com.google.gson.Gson;
         AmazonEchoControlHandlerFactory.class }, configurationPid = "binding.amazonechocontrol")
 @NonNullByDefault
 public class AmazonEchoControlHandlerFactory extends BaseThingHandlerFactory {
-    private final Logger logger = LoggerFactory.getLogger(AmazonEchoControlHandlerFactory.class);
-
     private final Set<AccountHandler> accountHandlers = new HashSet<>();
     private final HttpService httpService;
     private final StorageService storageService;

bundles/org.smarthomej.binding.amazonechocontrol/src/main/java/org/smarthomej/binding/amazonechocontrol/internal/AmazonEchoDynamicStateDescriptionProvider.java
@@ -35,6 +35,7 @@ import org.openhab.core.types.StateOption;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.smarthomej.binding.amazonechocontrol.internal.connection.Connection;
 import org.smarthomej.binding.amazonechocontrol.internal.handler.AccountHandler;
 import org.smarthomej.binding.amazonechocontrol.internal.handler.EchoHandler;
 import org.smarthomej.binding.amazonechocontrol.internal.handler.FlashBriefingProfileHandler;

bundles/org.smarthomej.binding.amazonechocontrol/src/main/java/org/smarthomej/binding/amazonechocontrol/internal/ConnectionException.java
@@ -14,6 +14,7 @@
 package org.smarthomej.binding.amazonechocontrol.internal;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 
 /**
  * The {@link ConnectionException} is used for errors in the connection to the amazon server
@@ -21,11 +22,15 @@ import org.eclipse.jdt.annotation.NonNullByDefault;
  * @author Michael Geramb - Initial contribution
  */
 @NonNullByDefault
-public class ConnectionException extends RuntimeException {
+public class ConnectionException extends Exception {
 
     private static final long serialVersionUID = 1L;
 
     public ConnectionException(String message) {
         super(message);
+    }
+
+    public ConnectionException(@Nullable String message, @Nullable Throwable cause) {
+        super(message, cause);
     }
 }

bundles/org.smarthomej.binding.amazonechocontrol/src/main/java/org/smarthomej/binding/amazonechocontrol/internal/HttpException.java
@@ -1,37 +1,0 @@
-/**
- * Copyright (c) 2010-2021 Contributors to the openHAB project
- * Copyright (c) 2021 Contributors to the SmartHome/J project
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0
- *
- * SPDX-License-Identifier: EPL-2.0
- */
-package org.smarthomej.binding.amazonechocontrol.internal;
-
-import org.eclipse.jdt.annotation.NonNullByDefault;
-
-/**
- * The {@link HttpException} is used for http error codes
- *
- * @author Michael Geramb - Initial contribution
- */
-@NonNullByDefault
-public class HttpException extends RuntimeException {
-
-    private static final long serialVersionUID = 1L;
-    int code;
-
-    public int getCode() {
-        return code;
-    }
-
-    public HttpException(int code, String message) {
-        super(message);
-        this.code = code;
-    }
-}

bundles/org.smarthomej.binding.amazonechocontrol/src/main/java/org/smarthomej/binding/amazonechocontrol/internal/channelhandler/ChannelHandler.java
@@ -13,15 +13,13 @@
  */
 package org.smarthomej.binding.amazonechocontrol.internal.channelhandler;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.core.types.Command;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.smarthomej.binding.amazonechocontrol.internal.Connection;
+import org.smarthomej.binding.amazonechocontrol.internal.ConnectionException;
+import org.smarthomej.binding.amazonechocontrol.internal.connection.Connection;
 import org.smarthomej.binding.amazonechocontrol.internal.jsons.JsonDevices.Device;
 
 import com.google.gson.Gson;
@@ -36,7 +34,7 @@ import com.google.gson.JsonSyntaxException;
 public abstract class ChannelHandler {
 
     public abstract boolean tryHandleCommand(Device device, Connection connection, String channelId, Command command)
-            throws IOException, URISyntaxException, InterruptedException;
+            throws ConnectionException;
 
     protected final IAmazonThingHandler thingHandler;
     protected final Gson gson;

bundles/org.smarthomej.binding.amazonechocontrol/src/main/java/org/smarthomej/binding/amazonechocontrol/internal/channelhandler/ChannelHandlerAnnouncement.java
@@ -21,7 +21,8 @@ import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.core.library.types.StringType;
 import org.openhab.core.types.Command;
-import org.smarthomej.binding.amazonechocontrol.internal.Connection;
+import org.smarthomej.binding.amazonechocontrol.internal.ConnectionException;
+import org.smarthomej.binding.amazonechocontrol.internal.connection.Connection;
 import org.smarthomej.binding.amazonechocontrol.internal.jsons.JsonDevices.Device;
 
 import com.google.gson.Gson;
@@ -47,53 +48,57 @@ public class ChannelHandlerAnnouncement extends ChannelHandler {
 
     @Override
     public boolean tryHandleCommand(Device device, Connection connection, String channelId, Command command)
-            throws IOException, URISyntaxException {
-        if (channelId.equals(CHANNEL_NAME)) {
-            if (command instanceof StringType) {
-                String commandValue = ((StringType) command).toFullString();
-                String body = commandValue;
-                String title = null;
-                String speak = commandValue;
-                Integer volume = null;
-                if (commandValue.startsWith("{") && commandValue.endsWith("}")) {
-                    try {
-                        AnnouncementRequestJson request = parseJson(commandValue, AnnouncementRequestJson.class);
-                        if (request != null) {
-                            speak = request.speak;
-                            if (speak == null || speak.length() == 0) {
-                                speak = "."; // dot generates a beep
-                            }
-                            volume = request.volume;
-                            title = request.title;
-                            body = request.body;
-                            if (body == null) {
-                                body = speak;
-                            }
-                            Boolean sound = request.sound;
-                            if (sound != null) {
-                                if (!sound && !speak.startsWith("<speak>")) {
-                                    speak = "<speak>" + speak + "</speak>";
+            throws ConnectionException {
+        try {
+            if (channelId.equals(CHANNEL_NAME)) {
+                if (command instanceof StringType) {
+                    String commandValue = ((StringType) command).toFullString();
+                    String body = commandValue;
+                    String title = null;
+                    String speak = commandValue;
+                    Integer volume = null;
+                    if (commandValue.startsWith("{") && commandValue.endsWith("}")) {
+                        try {
+                            AnnouncementRequestJson request = parseJson(commandValue, AnnouncementRequestJson.class);
+                            if (request != null) {
+                                speak = request.speak;
+                                if (speak == null || speak.length() == 0) {
+                                    speak = "."; // blank generates a beep
                                 }
-                                if (sound && speak.startsWith("<speak>")) {
-                                    body = "Error: The combination of sound and speak in SSML syntax is not allowed";
-                                    title = "Error";
-                                    speak = "<speak><lang xml:lang=\"en-UK\">Error: The combination of sound and speak in <prosody rate=\"x-slow\"><say-as interpret-as=\"characters\">SSML</say-as></prosody> syntax is not allowed</lang></speak>";
+                                volume = request.volume;
+                                title = request.title;
+                                body = request.body;
+                                if (body == null) {
+                                    body = speak;
+                                }
+                                Boolean sound = request.sound;
+                                if (sound != null) {
+                                    if (!sound && !speak.startsWith("<speak>")) {
+                                        speak = "<speak>" + speak + "</speak>";
+                                    }
+                                    if (sound && speak.startsWith("<speak>")) {
+                                        body = "Error: The combination of sound and speak in SSML syntax is not allowed";
+                                        title = "Error";
+                                        speak = "<speak><lang xml:lang=\"en-UK\">Error: The combination of sound and speak in <prosody rate=\"x-slow\"><say-as interpret-as=\"characters\">SSML</say-as></prosody> syntax is not allowed</lang></speak>";
+                                    }
+                                }
+                                if ("<speak> </speak>".equals(speak)) {
+                                    volume = -1; // Do not change volume
                                 }
                             }
-                            if ("<speak> </speak>".equals(speak)) {
-                                volume = -1; // Do not change volume
-                            }
+                        } catch (JsonSyntaxException e) {
+                            body = "Invalid Json." + e.getLocalizedMessage();
+                            title = "Error";
+                            speak = "<speak><lang xml:lang=\"en-US\">" + body + "</lang></speak>";
+                            body = e.getLocalizedMessage();
                         }
-                    } catch (JsonSyntaxException e) {
-                        body = "Invalid Json." + e.getLocalizedMessage();
-                        title = "Error";
-                        speak = "<speak><lang xml:lang=\"en-US\">" + body + "</lang></speak>";
-                        body = e.getLocalizedMessage();
                     }
+                    thingHandler.startAnnouncement(device, speak, Objects.requireNonNullElse(body, ""), title, volume);
                 }
-                thingHandler.startAnnouncement(device, speak, Objects.requireNonNullElse(body, ""), title, volume);
+                refreshChannel();
             }
-            refreshChannel();
+        } catch (IOException | URISyntaxException e) {
+            throw new ConnectionException(e.getMessage(), e);
         }
         return false;
     }

commit b311997234336a5326be6aaa14d2e81f632356d9

[amazonechocontrol] fix first-time login
This was reported on the forum and is a regression introduced in #237 (wrong ordering of constructor parameters),

Signed-off-by: Jan N. Klug <github@klug.nrw>

bundles/org.smarthomej.binding.amazonechocontrol/src/main/java/org/smarthomej/binding/amazonechocontrol/internal/connection/LoginData.java
@@ -51,7 +51,7 @@ public class LoginData {
     public @Nullable Date loginTime;
     private List<Cookie> cookies = new ArrayList<>();
 
-    public LoginData(CookieManager cookieManager, String frc, String serial, String deviceId) {
+    public LoginData(CookieManager cookieManager, String deviceId, String frc, String serial) {
         this.cookieManager = cookieManager;
         this.frc = frc;
         this.serial = serial;