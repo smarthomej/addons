626a62fbb51e35c04e7b2328c3903e62631fb504
Jan N. Klug <github@klug.nrw>
2022-01-26 19:55:31 +0100

    fix Configuration issue
    Signed-off-by: Jan N. Klug <github@klug.nrw>

---
+++ bundles/org.smarthomej.binding.amazonechocontrol/src/main/java/org/smarthomej/binding/amazonechocontrol/internal/smarthome/ColorChannelConfig.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2021-2022 Contributors to the SmartHome/J project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.smarthomej.binding.amazonechocontrol.internal.smarthome;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The {@link ColorChannelConfig} holds the configuration for a color channel
+ *
+ * @author Jan N. Klug - Initial contribution
+ */
+@NonNullByDefault
+public class ColorChannelConfig {
+    public boolean matchColors = false;
+}
