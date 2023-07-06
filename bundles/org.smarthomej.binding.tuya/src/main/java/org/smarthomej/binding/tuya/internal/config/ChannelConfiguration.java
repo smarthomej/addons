ff4b37018359062f47f9576d7358c41d3e5a5fc6
Dmitry P. (d51x) <dimonich110@gmail.com>
2023-07-06 10:16:44 +0300

    [tuya] IR Controller: update docs
    Signed-off-by: Dmitry P. (d51x) <dimonich110@gmail.com>

--- bundles/org.smarthomej.binding.tuya/src/main/java/org/smarthomej/binding/tuya/internal/config/ChannelConfiguration.java
+++ bundles/org.smarthomej.binding.tuya/src/main/java/org/smarthomej/binding/tuya/internal/config/ChannelConfiguration.java
@@ -30,6 +30,5 @@ public class ChannelConfiguration {
     public int irSendDelay = 300;
     public int irCodeType = 0;
     public String irType = "";
-
     public Boolean activeListen = Boolean.FALSE;
 }
