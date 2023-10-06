a27c3b6221cfc6415012f5e651b5239ec09c7944
clinique <gael@lhopital.org>
2023-10-06 10:09:19 +0200

    Missing an import
    Signed-off-by: clinique <gael@lhopital.org>

--- bundles/org.smarthomej.automation.javarule/src/main/java/org/smarthomej/automation/javarule/internal/script/RuleProcessor.java
+++ bundles/org.smarthomej.automation.javarule/src/main/java/org/smarthomej/automation/javarule/internal/script/RuleProcessor.java
@@ -16,6 +16,8 @@ import static org.smarthomej.automation.javarule.internal.JavaRuleConstants.ANNO
 import static org.smarthomej.automation.javarule.internal.JavaRuleConstants.CONDITION_FROM_ANNOTATION;
 import static org.smarthomej.automation.javarule.internal.JavaRuleConstants.TRIGGER_FROM_ANNOTATION;
 
+import java.util.Set;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -78,7 +80,7 @@ public class RuleProcessor {
 
             String ruleName = method.getName();
             String ruleDescription = ruleAnnotation.name();
-            Set<String> tags = ruleAnnotation.tags();
+            Set<String> ruleTags = ruleAnnotation.tags();
             
             if (ruleDescription.isBlank() || ANNOTATION_DEFAULT.equals(ruleDescription)) {
                 ruleDescription = script.getClass().getSimpleName() + "/" + method.getName();
@@ -123,7 +125,7 @@ public class RuleProcessor {
             simpleRule.setDescription(ruleDescription);
             simpleRule.setTriggers(triggers);
             simpleRule.setConditions(conditions);
-            simpleRule.setTags()
+            simpleRule.setTags(ruleTags);
 
             rules.add(simpleRule);
 
