3c79448ae12508a71bd00c53de9deb12333bc037
Ronny Grun <ronny.grun@t-online.de>
2025-03-24 10:59:59 +0100

    [viessmann] further work for API v2
    Signed-off-by: Ronny Grun <ronny.grun@t-online.de>

--- bundles/org.smarthomej.binding.viessmann/src/main/java/org/smarthomej/binding/viessmann/internal/api/ViessmannApi.java
+++ bundles/org.smarthomej.binding.viessmann/src/main/java/org/smarthomej/binding/viessmann/internal/api/ViessmannApi.java
@@ -206,7 +206,7 @@ public class ViessmannApi {
     }
 
     public @Nullable DeviceDTO getAllDevices() throws ViessmannCommunicationException {
-        String response = executeGet(VIESSMANN_BASE_URL + "iot/v1/equipment/installations/" + installationId
+        String response = executeGet(VIESSMANN_BASE_URL + "iot/v2/equipment/installations/" + installationId
                 + "/gateways/" + gatewaySerial + "/devices");
         return GSON.fromJson(response, DeviceDTO.class);
     }
@@ -235,7 +235,7 @@ public class ViessmannApi {
 
     private void setInstallationAndGatewayId() {
         try {
-            String response = executeGet(VIESSMANN_BASE_URL + "iot/v1/equipment/installations?includeGateways=true");
+            String response = executeGet(VIESSMANN_BASE_URL + "iot/v2/equipment/installations?includeGateways=true");
             InstallationDTO installation = GSON.fromJson(response, InstallationDTO.class);
             if (installation != null) {
                 List<Data> listData = installation.data;
